89f821d3863dcfd628f1f19e885ab7f0d938816d
tzuhao84 <67537684+tzuhao84@users.noreply.github.com>
2022-02-06 21:45:35 +0800

    move ('Please enter your ID')>> input()

--- a020.py
+++ a020.py
@@ -1,7 +1,7 @@
 id_dict={'A':10,'B':11,'C':12,'D':13,'E':14,'F':15,'G':16,'H':17,'I':34,'J':18,'K':19,'L':20,
               'M':21,'N':22,'O':35,'P':23,'Q':24,'R':25,'S':26,'T':27,'U':28,'V':29,'W':32,'X':30,'Y':31,'Z':33}
-print('Please enter your ID:')
-ID = list(map(str,input()))
+#print('Please enter your ID:')
+ID = list(map(str,input('please enter your ID number:\n')))
 # print(ID)
 number = str(id_dict[ID[0]])  # transform the alphabet into number
 units=list(map(str,number))[1]
