904a0ef05688bd2625a05a44d2be9ba8e816c95d
tzuhao84 <67537684+tzuhao84@users.noreply.github.com>
2022-02-13 16:10:49 +0800

    a021 final answer

--- a021.py
+++ a021.py
@@ -1,3 +1,18 @@
 # tzuhao's soludtion for zerojudge a021 
 
 
+
+ls=list(map(str,input().split()))
+# print(ls)
+a=int(ls[0])
+b=int(ls[2])
+
+if ls[1]=='+':
+    print(a+b)
+if ls[1]=='-':
+    print(a-b)
+if ls[1]=='*':
+    print(a*b)
+if ls[1]=='/':
+    print(a//b)
+
