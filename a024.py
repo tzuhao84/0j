71b224aa35fedc3eb2d29606b9d252bf5134d1d8
tzuhao84 <67537684+tzuhao84@users.noreply.github.com>
2022-02-13 17:34:36 +0800

    first commit

---
+++ a024.py
@@ -0,0 +1,23 @@
+def is_prime(n):
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+    return True
+
+
+quiz=list(map(int,input().split()))
+a=quiz[0]
+b=quiz[1]
+c=min(a,b)
+
+# print(is_prime(a),is_prime(b))
+# print(c)
+
+if is_prime(a) or is_prime(b):
+    print(1)
+else:
+    for i in range(c,0,-1):
+        if a%i==0 and b%i==0:
+            GCD=i 
+            break
+    print(GCD) 
