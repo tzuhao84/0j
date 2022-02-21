86969ff8d101886b0ca214337dcd6173cde294b9
tzuhao84 <67537684+tzuhao84@users.noreply.github.com>
2022-02-21 21:49:32 +0800

    a034 final

---
+++ a034.py
@@ -0,0 +1,38 @@
+# tzuhao solution for zerojudge a034(binary)
+
+while True:
+    try:
+        dec=int(input())
+        index=0
+        while(dec//(2**index)>1):
+            # print('index=',index)
+            # print('dec//(2**index)=',dec//(2**index))
+            index=index+1   
+        # print('highest digit=',index)
+
+        modulus=1
+        binarylist=[]
+        # while(modulus!=0):
+        #     modulus=dec%(2**index)
+        #     print('modulus=',modulus)
+        #     tobinary=dec//(2**index)
+        #     binarylist.append(tobinary)
+        #     dec=modulus
+        #     print('dec=',dec)
+        #     index=index-1
+        for i in range(index,-1,-1):
+            modulus=dec%(2**index)
+            # print('modulus=',modulus)
+            tobinary=dec//(2**index)
+            binarylist.append(tobinary)
+            dec=modulus
+            # print('dec=',dec)
+            index=index-1
+
+        binarylist=[str(i) for i in binarylist]
+        # print('binarylist=',binarylist)
+        s="".join(binarylist)
+        print(s)
+
+    except:
+        break
